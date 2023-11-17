232fe359e01dab1e826f608b58764dd11c96d225
zed <zedsmail@163.com>
2023-11-17 17:12:48 +0800

    --

--- module/test/module.go
+++ module/test/module.go
@@ -3,7 +3,6 @@ package test
 import (
 	"github.com/zeddy-go/core/container"
 	"github.com/zeddy-go/core/contract"
-	"template/module/test/domain"
 	"template/module/test/handler"
 	"template/module/test/infra/migration"
 	"template/module/test/infra/repository"
@@ -12,14 +11,10 @@ import (
 type Module struct{}
 
 func (m Module) RegisterRoute(r contract.IRouter) {
-	userRepository, err := container.Resolve[domain.IUserRepository]()
-	if err != nil {
-		panic(err)
-	}
-
-	testHandler := handler.NewTestHandler(userRepository)
-	r.GET("/test", testHandler.TestGet)
-	r.POST("/test", testHandler.TestPost)
+	container.Invoke(func(testHandler *handler.TestHandler) {
+		r.GET("/test", testHandler.TestGet)
+		r.POST("/test", testHandler.TestPost)
+	})
 }
 
 func (m Module) Init() {
@@ -30,6 +25,7 @@ func (m Module) Init() {
 	})
 
 	container.Register(repository.NewUserRepository)
+	container.Register(handler.NewTestHandler)
 
 	container.Invoke(migration.RegisterMigration)
 }
